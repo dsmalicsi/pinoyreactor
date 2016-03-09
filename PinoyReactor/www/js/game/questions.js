3875517fdef45c0140f0d120110e475e499efe9f
Daryll Malicsi <daryll.malicsi@perpetualwave.com>
2016-03-09 17:28:09 +0800

    Added game logic and separated questions file

---
+++ PinoyReactor/www/js/game/questions.js
@@ -0,0 +1,31 @@
+
+    var mathProblems = {
+      "2 + 2 = 4":true,
+      "2 + 5 = 1":false,
+      "2 + 1 = 7":false,
+      "2 - 1 = 1":true,
+      "5 / 1 = 3":false,
+      "15 / 20 = 13":false,
+      "15 - 20 = -5":true,
+      "15 + 20 = 35":true,
+      "6 + 20 = 26":true,
+      "6 + 20 = 22":false,
+      "8 + 21 = 22":false,
+      "1 + 10 = 13":false,
+      "100 + 20 = 120":true
+    }
+    
+    var pinoyTrivia = {
+      "Si <i>Jose Rizal</i> ang pambansang bayani":true,
+      "Si <i>Jose Rizal</i> ang bumuo ng KKK":false,
+      "Si <i>Apolinario Mabini</i> ang pambansang bayani":false,
+      "Si <i>Apolinario Mabini</i> ang dakilang lumpo":true,
+      "Si <i>Andres Bonifacio</i> ang pambansang bayani":false,
+      "Si <i>Andres Bonifacio</i> ang unang presidente":false,
+      "Si <i>Andres Bonifacio</i> ang bumuo ng KKK":true,
+      "Si <i>Ninoy Aquino</i> ang naging presidente ng Pilipinas noong 2010-2016":false,
+      "Si <i>Ninoy Aquino</i> ang kapatid ni Jose Rizal":false,
+      "Si <i>Noynoy Aquino</i> ang naging presidente ng Pilipinas noong 2010-2016":true
+    }
+    
+    
